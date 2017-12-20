d28b2246f9dc4973ebdad40e94398e8bac176763
jitendra <jitendra.auxesis@outlook.com>
2017-12-20 19:36:48 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,7 +23,9 @@ export class AppComponent {
     //localStorage.setItem("AUXUserUrl","http://139.59.9.73:8000");//"http://192.168.0.116:8000/");
     // main "http://104.236.95.166:8000/"http://104.236.95.166:8000/
     // staging "http://139.59.156.145:8000/"
-    this.storage.store("AUXUserUrl","http://104.236.95.166/");//"http://139.59.156.145:8000/");
+
+    //main ssl https://massico.auxledger.org/
+    this.storage.store("AUXUserUrl","https://massico.auxledger.org/");//"http://139.59.156.145:8000/");
   }
 
   ngOnInit() {
